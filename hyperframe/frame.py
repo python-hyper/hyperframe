f972a8276db9b64375c5c246d3d453442b0a6926
Seth Michael Larson <sethmichaellarson@protonmail.com>
2016-09-28 15:48:24 -0500

    Network order is slower than big order

--- hyperframe/frame.py
+++ hyperframe/frame.py
@@ -29,13 +29,13 @@ _STREAM_ASSOC_NO_STREAM = "no-stream"
 _STREAM_ASSOC_EITHER = "either"
 
 # Structs for packing and unpacking
-_STRUCT_HBBBL = struct.Struct("!HBBBL")
-_STRUCT_LL = struct.Struct("!LL")
-_STRUCT_HL = struct.Struct("!HL")
-_STRUCT_LB = struct.Struct("!LB")
-_STRUCT_L = struct.Struct("!L")
-_STRUCT_H = struct.Struct("!H")
-_STRUCT_B = struct.Struct("!B")
+_STRUCT_HBBBL = struct.Struct(">HBBBL")
+_STRUCT_LL = struct.Struct(">LL")
+_STRUCT_HL = struct.Struct(">HL")
+_STRUCT_LB = struct.Struct(">LB")
+_STRUCT_L = struct.Struct(">L")
+_STRUCT_H = struct.Struct(">H")
+_STRUCT_B = struct.Struct(">B")
 
 
 class Frame(object):
